c772a371dc79327282c82051378a2fef3da713ef
Seth <zzbfor25@gmail.com>
2026-01-28 22:07:28 +0900

    Add jwt authentication for reservations

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -2,8 +2,10 @@ import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { UserDocument } from './users/models/user.schema';
-import { CurrentUser } from './current-user.decorator';
+import { CurrentUser } from '@app/common';
 import type { Response } from 'express';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -15,4 +17,11 @@ export class AuthController {
     return this.authService.login(user, response);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @MessagePattern('authenticate')
+  async authenticate(
+    @Payload() data: any,
+  ) {
+    return data.user
+  }
 }
--- apps/auth/src/strategies/jwt.strategy.ts
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -3,7 +3,6 @@ import { PassportStrategy } from "@nestjs/passport";
 import { UsersService } from "../users/users.service";
 import { ConfigService } from "@nestjs/config";
 import { ExtractJwt, Strategy } from "passport-jwt";
-import { Request } from "express";
 import { TokenPayload } from "../interfaces/token-payload.interface";
 
 @Injectable()
@@ -19,7 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
         super({
             jwtFromRequest: ExtractJwt.fromExtractors([
-                (request: Request) => request?.cookies?.Authentication,
+                (request: any) => request?.cookies?.Authentication || request?.Authentication,
             ]),
             secretOrKey: secret,
         });
